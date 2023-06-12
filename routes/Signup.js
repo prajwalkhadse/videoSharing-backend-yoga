c741085b9b7c17da26fa9226af7055b425e25236
Yogesh J. Baiskar <68940642+YJB28@users.noreply.github.com>
2023-06-12 14:31:46 +0530

    Update Signup.js
    upadte signuop.js

--- routes/Signup.js
+++ routes/Signup.js
@@ -1,44 +1,88 @@
+// const express = require('express');
+// const router = express.Router()
+
+// const mongoose = require('mongoose')
+// const bcrypt = require('bcrypt')
+
+// const User = require('../models/User')
+
+// router.post("/register", (req, res) => {
+//     console.log("prajwal");
+//     let data = req.body
+//     console.log(data)
+//     bcrypt.hash(data.password, 10).then((encryptpin) => {
+//         const user = new User({
+//             name: data.name,
+//             email: data.email,
+//             password: encryptpin,
+//             phone: data.phone,
+//             profession: data.profession
+
+//         })
+//         // console.log(user)
+//         user.save().then((record) => {
+//             res.status(201).json({
+//                 message: "Registerd Successfully !",
+//                 data: record,
+//             })
+//         }).catch((error) => {
+//             res.status(500).json({
+//                 message: "Error during registration !",
+//                 error: error
+//             })
+//         })
+//     }).catch((error) => {
+//         res.status(500).json({
+//             message: "Error during registration with PIN !",
+//             error: error
+//         })
+//     })
+
+
+// })
+
+// module.exports = router
+
+
+
+//new code
 const express = require('express');
-const router = express.Router()
+const router = express.Router();
+const User = require('../models/User');
 
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
+router.post('/', async (req, res) => {
+  const { name, email, phone, profession, password, confirmPassword } = req.body;
 
-const User = require('../models/User')
+  // Check if passwords match
+  if (password !== confirmPassword) {
+    return res.status(400).json({ error: "Password does not match with confirm password" });
+  }
 
-router.post("/register", (req, res) => {
-    console.log("prajwal");
-    let data = req.body
-    console.log(data)
-    bcrypt.hash(data.password, 10).then((encryptpin) => {
-        const user = new User({
-            name: data.name,
-            email: data.email,
-            password: encryptpin,
-            phone: data.phone,
-            profession: data.profession
+  try {
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: "User with this email already exists" });
+    }
 
-        })
-        // console.log(user)
-        user.save().then((record) => {
-            res.status(201).json({
-                message: "Registerd Successfully !",
-                data: record,
-            })
-        }).catch((error) => {
-            res.status(500).json({
-                message: "Error during registration !",
-                error: error
-            })
-        })
-    }).catch((error) => {
-        res.status(500).json({
-            message: "Error during registration with PIN !",
-            error: error
-        })
-    })
+    // Create a new user
+    const user = new User({
+      name,
+      email,
+      phone,
+      profession,
+      password
+    });
 
+    // Save the user to the database
+    await user.save();
 
-})
+    // Send response
+    res.status(201).json({ message: "Registration successful" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "An error occurred during registration" });
+  }
+});
 
-module.exports = router
+module.exports = router;
